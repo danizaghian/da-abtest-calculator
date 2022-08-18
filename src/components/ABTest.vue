8d9ad3343ab009c6a08acbbe3ac7b11a8debcc36
Danielle Zaghian <daniellezaghian@Danielles-MacBook-Pro.local>
2022-08-18 13:16:47 -0700

    adds details of text weights and colors

--- src/components/ABTest.vue
+++ src/components/ABTest.vue
@@ -9,7 +9,9 @@
     <div class="container">
       <!-- Start Overview -->
       <div class="row mb-4">
-        <h5 class="mb-3">Setting up an AB Test: What sample size do I need?</h5>
+        <h5 class="mb-3 fw-bold">
+          Setting up an AB Test: What sample size do I need?
+        </h5>
         <p>
           Setting up a proper A/B experiment involves selecting the appropriate
           sample size. This ensures the test is adequately powered to detect the
@@ -24,7 +26,7 @@
       <!-- Start Summaries -->
       <div class="row mb-5">
         <div class="col">
-          <h5 class="mb-3">Baseline Conversion Rate</h5>
+          <h5 class="mb-3 fw-bold">Baseline Conversion Rate</h5>
           <p>
             The preexisting or expected conversion rate of the control group.
             This could be a click-through ra te, retention rate, or positive
@@ -32,7 +34,7 @@
           </p>
         </div>
         <div class="col">
-          <h5 class="mb-3">Minimum Detectable Effect</h5>
+          <h5 class="mb-3 fw-bold">Minimum Detectable Effect</h5>
           <p>
             The <strong>relative</strong> minimum difference in conversion rate
             between the test and control group we want to observe. This can be
@@ -45,7 +47,7 @@
       <!-- Start Sliders -->
       <div class="row mb-5">
         <div class="col text-center">
-          <h2>{{ bcr }}</h2>
+          <h2 class="fw-bold">{{ bcr }}%</h2>
 
           <input
             type="range"
@@ -57,7 +59,7 @@
           />
         </div>
         <div class="col text-center">
-          <h2>{{ mde }}</h2>
+          <h2 class="fw-bold">{{ mde }}%</h2>
           <!-- <label for="mde">Minimum Detectable Effect</label><br /> -->
 
           <input
@@ -74,7 +76,9 @@
       <!-- Start Conversion Rate -->
       <div class="row mb-5">
         <p class="lead text-center text-bg-light p-4">
-          We want to detect a new conversion rate &lt; 6.0% or > 10.0%
+          We want to detect a new conversion rate &lt;
+          <strong class="text-podium">6.0%</strong> or >
+          <strong class="text-podium">10.0%</strong>
         </p>
       </div>
       <!-- End Conversion Rate -->
@@ -82,16 +86,16 @@
       <div class="row text-center text-bg-light p-4 border">
         <div class="col">
           <h5 class="mt-3">Test Size</h5>
-          <h1>{{ testSize }}</h1>
+          <h1>{{ testSize }}K</h1>
         </div>
         <div class="col">
           <h5 class="mt-3">Control Size</h5>
-          <h1>{{ controlSize }}</h1>
+          <h1>{{ controlSize }}K</h1>
         </div>
       </div>
       <div class="row text-center text-bg-podium p-4 border mb-5">
         <h5 class="mt-3">Total Sample Size</h5>
-        <h1>{{ testSize + controlSize }}</h1>
+        <h1>{{ testSize + controlSize }}K</h1>
       </div>
       <!-- End Sizes -->
     </div>
